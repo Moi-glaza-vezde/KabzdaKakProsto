3c67701c6bd0fad2767f6e093dffeebfc4b98122
Moi-glaza-vezde <moi.glaza.vezde@gmail.com>
2025-02-11 11:11:24 +0500

    done select

--- src/components/select/Select.stories.tsx
+++ src/components/select/Select.stories.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Select} from './Select';
-
+import {action} from '@storybook/addon-actions';
 
 
 export default {
@@ -8,4 +8,32 @@ export default {
     component: Select,
 };
 
-export const SelectMode=()=>{}
+export const SelectStart = () => {
+const [value,setValue] = useState('2');
+    return (
+        <Select onChange={setValue}
+                value={value}
+                items={
+                    [
+                        {value: '1', title: 'Minsk'},
+                        {value: '2', title: 'Moscow'},
+                        {value: '3', title: 'Kiev'},
+                    ]
+                }/>
+    )
+}
+export const WithoutValue = () => {
+    const [value,setValue] = useState(null);
+
+    return (
+        <Select onChange={setValue}
+                value={value}
+                items={
+                    [
+                        {value: '1', title: 'Minsk'},
+                        {value: '2', title: 'Moscow'},
+                        {value: '3', title: 'Kiev'},
+                    ]
+                }/>
+    )
+}
